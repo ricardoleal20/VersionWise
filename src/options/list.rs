dfb5e15cea95db43be9f74946e465463278c62d0
ricardoleal20 <rick.leal420@gmail.com>
2024-03-12 00:44:52 -0600

    🚧 WIP: Include the `list` option

--- src/options/list.rs
+++ src/options/list.rs
@@ -1,12 +1,49 @@
 /// ================================ ///
 ///         OPTIONS :: List          ///
 /// ================================ ///
+use colored::*;
+use std::collections::HashSet;
 // Local imports
 use crate::utilities::get_current_changesets;
 
 pub fn list_changesets() {
     // Get the changesets and list them
     let changesets = get_current_changesets();
+    // Add a vec of tags that were visited
+    let mut printed_tags: HashSet<&String> = HashSet::new();
     // Process them
-    println!("{:?}", changesets);
+    for change_type in ["MAJOR", "MINOR", "PATCH"] {
+        // Print the first layer
+
+        // Filter the changesets for all those that match the change type
+        for changeset in changesets
+            .iter()
+            .filter(|c| c.change == format!("\t{}", change_type))
+        {
+            // If this tag has been printed already, then continue
+            if printed_tags.contains(&changeset.tag) {
+                continue;
+            }
+            // Print the tag if has not been printed already
+            println!("- [{}]", changeset.tag.green());
+            // And now, filter all the changesets for the tag and the change type
+            for nested_changeset in changesets
+                .iter()
+                .filter(|c| c.change == format!("\t{}", change_type) && c.tag == changeset.tag)
+            {
+                // If this changeset has a module, include it. If not, then just don't
+                if nested_changeset.module == "" {
+                    println!("    - {}", nested_changeset.message);
+                } else {
+                    println!(
+                        "    - {}: {}",
+                        nested_changeset.module.blue(),
+                        nested_changeset.message
+                    );
+                }
+            }
+            // And include this tag in the printed ones
+            printed_tags.insert(&changeset.tag);
+        }
+    }
 }
--- src/utilities/changesets_utilities.rs
+++ src/utilities/changesets_utilities.rs
@@ -17,19 +17,33 @@ fn parse_changeset(file_name: &str, file_content: &str) -> Option<Changeset> {
 
     // Get the data from the changeset
     if remaining_lines.len() >= 3 {
-        let change = remaining_lines[1].trim().to_string();
-        let mut module = remaining_lines[3].split(":").collect();
-        let message = remaining_lines[3..].join("\n").trim().to_string();
-        // If module and message are equal, then they didn't have a module
+        let mut module_parts = remaining_lines[1].split(":");
+        let change = module_parts.next().unwrap().to_string();
+        let tag = module_parts.next().unwrap().to_string();
+        // And add the module and message
+        let mut module_message = remaining_lines[3].split(":");
+        // Get the strings comming from the module message next element
+        let mut module = match module_message.next() {
+            Some(module_str) => module_str.trim().to_string(),
+            None => String::new(), // Include a new empty string for the next scenario
+        };
+        let message = match module_message.next() {
+            Some(message_str) => message_str.trim().to_string(),
+            None => {
+                // If there's no second element, clone the module one
+                module.clone()
+            }
+        };
+        // If the module and the message are the same, make the module an empty value
         if module == message {
-            module = format!("")
+            module = "".to_string()
         }
-
         // Create it
         Some(Changeset::new(
             file_name.to_string().split(".md").collect(),
             change,
             module,
+            tag,
             message,
         ))
     } else {
